f036652fe58b290caffc744321f9359f51a8c9c9
Isjhar Kautsar <isjhar@gmail.com>
2018-04-02 09:10:07 +0700

    implement v1.0

---
+++ src/tugas_besar_uas/Tugas_Besar_UAS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_besar_uas;
+
+
+/*
+ * FileChooserDemo2.java requires these files:
+ *   ImageFileView.java
+ *   ImageFilter.java
+ *   ImagePreview.java
+ *   Utils.java
+ *   images/jpgIcon.gif (required by ImageFileView.java)
+ *   images/gifIcon.gif (required by ImageFileView.java)
+ *   images/tiffIcon.gif (required by ImageFileView.java)
+ *   images/pngIcon.png (required by ImageFileView.java)
+ */
+
+/**
+ *
+ * @author kokon
+ */
+public class Tugas_Besar_UAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame1 gui = new NewJFrame1();
+        gui.show();
+//        System.out.println(Math.toDegrees(Math.acos(0)));
+    }
+
+    
+}
